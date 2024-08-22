413483ba36e29874d29f9877281d0d3c74e434fc
HIHJH <2170095@ewhain.net>
2024-08-22 10:15:49 +0900

    feat: 온보딩UI (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,49 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import BackgroundImage from '../data/unsplash.png';
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center p-4">
-      <h1 className="heading mb-4">순간을 chik다. ChikSnap</h1>
-      <p className="body-2 mb-24 text-gray-400">
-        나에게 맞는 사진작가를 추천받아요
-      </p>
-      <Link href="/select-type" className="btn-primary font-bold">
-        원하는 사진작가 찾아보기
-      </Link>
+    <div className="relative flex flex-col w-full h-screen p-[1rem]">
+      <div className="absolute inset-0 z-0">
+        <Image
+          src={BackgroundImage}
+          alt="background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="pointer-events-none"
+        />
+      </div>
+      {/* 배경 그라데이션 */}
+      <div className="absolute inset-0 z-10 bg-gradient-to-t from-black/80 via-black/70 to-transparent"></div>
+      <div className="relative z-20 h-full flex flex-col justify-between">
+        <div className="pl-[1.25rem]">
+          <h1 className="body-2 mt-[6.94rem] mb-[0.64rem] text-white">
+            순간을 chik다,
+            <br />
+            칙스냅
+          </h1>
+          <div className="w-[6.3125rem] h-[0.125rem] rounded-[12.5rem] bg-white/40" />
+        </div>
+        <div className="flex flex-col">
+          <div className="pl-[1.25rem]">
+            <h1 className="heading text-white mb-[1rem]">
+              스냅 작가님을
+              <br />
+              찾고 있으신가요?
+            </h1>
+            <p className="body-2 text-white mb-[3.25rem]">
+              나에게 맞는 스냅 작가님을 찾기
+              <br />
+              어려우셨다면 저희가 찾아드릴게요!
+            </p>
+          </div>
+          <Link href="/select-type" className="btn-default body-3">
+            원하는 사진작가 찾아보기
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
