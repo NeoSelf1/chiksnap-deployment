59d2fa03b4d4f408ea8a5cd0afcf35ab251cc280
HIHJH <2170095@ewhain.net>
2024-08-22 14:20:45 +0900

    feat: 헤더 사전알림신청 visible 설정(#2)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,18 +10,20 @@ import GrayLogo from '../data/grayLogo.svg';
 const Header = () => {
   const pathname = usePathname();
   const [isHomePage, setIsHomePage] = useState(pathname === '/');
+  const [isNotificationPage, setIsNotificationPage] = useState(
+    pathname === '/request-notification',
+  );
 
   useEffect(() => {
     setIsHomePage(pathname === '/');
+    setIsNotificationPage(pathname === '/request-notification');
   }, [pathname]);
 
   return (
     <header
       className={`${
-        isHomePage
-          ? 'fixed top-0 left-0 right-0 max-w-md mx-auto w-full bg-black/40 backdrop-blur-sm text-white'
-          : 'relative bg-gray-50'
-      } max-w-md mx-auto w-full px-[1rem] h-[3.5rem] flex justify-center z-50`}
+        isHomePage ? 'bg-black/40 backdrop-blur-sm text-white' : ' bg-gray-50'
+      } fixed top-0 left-0 right-0 max-w-md mx-auto w-full px-[1rem] h-[3.5rem] flex justify-center z-50`}
     >
       <div className="flex justify-center my-[1rem]">
         <Link href="/">
@@ -34,16 +36,18 @@ const Header = () => {
           />
         </Link>
       </div>
-      <div className="flex items-center">
-        <Link
-          href="/"
-          className={`${
-            isHomePage ? 'border-gray-700' : 'border-gray-100 bg-white'
-          } h-[2rem] p-[8px] rounded-[0.25rem] border absolute right-[1rem]`}
-        >
-          <p className="caption">사전알림신청</p>
-        </Link>
-      </div>
+      {!isNotificationPage && (
+        <div className="flex items-center">
+          <Link
+            href="/request-notification"
+            className={`${
+              isHomePage ? 'border-gray-700' : 'border-gray-100 bg-white'
+            } h-[2rem] p-[8px] rounded-[0.25rem] border absolute right-[1rem]`}
+          >
+            <p className="caption">사전알림신청</p>
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
