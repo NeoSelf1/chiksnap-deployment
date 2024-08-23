81a3f45418c96dfabbdebcddcdb263e9070df324
Hyeongseok Kim <hyeongseok@coconutsilo.com>
2024-08-24 04:32:24 +0900

    fix: 외부 이미지 도메인 추가 (#8)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['picsum.photos'], // 허용할 외부 이미지 도메인 추가
+    domains: [
+      'chik.s3.ap-northeast-2.amazonaws.com',
+      'scontent-gmp1-1.cdninstagram.com',
+    ], // 허용할 외부 이미지 도메인 추가
   },
 };
 
